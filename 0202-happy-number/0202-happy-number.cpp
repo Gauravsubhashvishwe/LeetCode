e1ae0d95168a7f78314db02c1bbc76f8923b5c51
Gaurav Vishwe <147568458+Gauravsubhashvishwe@users.noreply.github.com>
2026-07-23 00:04:12 +0530

    Time: 0 ms (100%), Space: 8.3 MB (38.96%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        unordered_set<int> seen;
+
+        while(n != 1 && !seen.count(n)){
+            seen.insert(n);
+            int result = 0;
+            while(n > 0){
+                result += ((n % 10) * (n % 10));
+                n /= 10;
+            }
+            n = result;
+        }
+        return n == 1;
+    }
+};
