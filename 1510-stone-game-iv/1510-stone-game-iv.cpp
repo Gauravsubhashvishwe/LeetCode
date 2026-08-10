c7cdf48dd1eab75da4190800d3208ee0bd6233ac
Gaurav Vishwe <147568458+Gauravsubhashvishwe@users.noreply.github.com>
2026-08-10 18:05:34 +0530

    Time: 31 ms (81.44%), Space: 8.9 MB (90.65%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.cpp
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -1,18 +1,26 @@
 class Solution {
-    int helper(int n, vector<int> &dp){
-        if(n < 1)return false;
-        if(dp[n] != -1)return dp[n];
-        dp[n] = 0;
-        for(int i = 1; i * i <= n; i++){
-            if(helper(n - i * i, dp) == 0){
-                dp[n] = 1;
+    // int helper(int n, vector<int> &dp){
+    //     if(n < 1)return false;
+    //     if(dp[n] != -1)return dp[n];
+    //     dp[n] = 0;
+    //     for(int i = 1; i * i <= n; i++){
+    //         if(helper(n - i * i, dp) == 0){
+    //             dp[n] = 1;
+    //         }
+    //     }
+    //     return dp[n];
+    // }
+public:
+    bool winnerSquareGame(int n) {
+        vector<bool> dp(n + 1, false);
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j * j <= i; j++){
+                if(!dp[i - j * j]){
+                    dp[i] = true;
+                    break;
+                }
             }
         }
         return dp[n];
-    }
-public:
-    bool winnerSquareGame(int n) {
-        vector<int> dp(n + 1, -1);
-        return helper(n, dp) == 1;
     }
 };
