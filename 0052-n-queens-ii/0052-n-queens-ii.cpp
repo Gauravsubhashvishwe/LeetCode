79ce0ce8a0f65c67207f9db1d59e1864a09b59d7
Gaurav Vishwe <147568458+Gauravsubhashvishwe@users.noreply.github.com>
2026-08-14 01:00:02 +0530

    Time: 2 ms (55.1%), Space: 8.1 MB (68.75%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -0,0 +1,51 @@
+class Solution {
+    bool is_safe(int i, int j, vector<string> &board){
+        for(int k = 0; k < i; k++){
+            if(board[k][j] == 'Q')return false;
+        }
+        int l = i - 1;
+        int m = j - 1;
+        while(l >= 0 && m >= 0){
+            if(board[l][m] == 'Q')return false;
+            l--;
+            m--;
+        }
+        l = i - 1;
+        m = j + 1;
+        while(l >= 0 && m < board.size()){
+            if(board[l][m] == 'Q')return false;
+            l--;
+            m++;
+        }
+        return true;
+    }
+
+    int helper(int l, vector<string> &board){
+        if(l == board.size()){
+            return 1;
+        }
+        int result = 0;
+        for(int i = 0; i < board.size(); i++){
+            if(is_safe(l, i, board)){
+                board[l][i] = 'Q';
+                result += helper(l + 1, board);
+                board[l][i] = '.';
+            }
+        }
+        return result;
+    }
+public:
+    int totalNQueens(int n) {
+        vector<string> board;
+        for(int i = 0; i < n; i++){
+            string l = "";
+            for(int j = 0; j < n; j++){
+                l += '.';
+            }
+            board.push_back(l);
+        }
+        int result;
+        result = helper(0, board);
+        return result;
+    }
+};
